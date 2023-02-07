22b7d20a088a72ed13ec828d275962149cd17969
Manash Roy <manashroy454@gmail.com>
2023-02-08 00:07:48 +0530

    change folder name

---
+++ Online codes by me/HackerRank/pattern.c
@@ -0,0 +1,41 @@
+/*
+4 4 4 4 4 4 4
+4 3 3 3 3 3 4
+4 3 2 2 2 3 4
+
+4 3 2 1 2 3 4
+4 3 2 2 2 3 4
+4 3 3 3 3 3 4
+4 4 4 4 4 4 4
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = n; j > i; j--)
+            printf("%d ", j);
+        for (int j = 1; j <= 2 * i - 1; j++)
+            printf("%d ", i);
+        for (int j = i + 1; j <= n; j++)
+            printf("%d ", j);
+        printf("\n");
+    }
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = n; j > i; j--)
+            printf("%d ", j);
+        for (int j = 1; j <= 2 * i - 1; j++)
+            printf("%d ", i + 1);
+        for (int j = i + 1; j <= n; j++)
+            printf("%d ", j);
+        printf("\n");
+    }
+
+    return 0;
+}
